b585edcb511b114cad10c80bc275b05731a8fbfe
张艳芳 <1363415880@qq.com>
2015-09-26 19:27:28 +0800

    sdasa

--- iFbeauty/iFbeauty/deleteViewController.m
+++ iFbeauty/iFbeauty/deleteViewController.m
@@ -16,8 +16,167 @@
 @implementation deleteViewController
 
 - (void)viewDidLoad {
-    [super viewDidLoad];
+    [super viewDidLoad];  // Do any additional setup after loading the view.
+    [self requestData];
+    
+    PFFile *userphoto = _item[@"photot"];
+    
+    //_userName.text =[NSString stringWithFormat:@"发帖人： %@", _ownername[@"secondname"]];
+    // NSLog(@"用户名%@",_userName.text);
+    
+    
+    _titlelabel.text = _item[@"title"];
+    _deLabel.text =[NSString stringWithFormat:@"%@", _item[@"detail"]];
+    //PFFile *photo = _ownername[@"photo"];
+    //    [photo getDataInBackgroundWithBlock:^(NSData *photoData, NSError *error) {
+    //        if (!error) {
+    //            UIImage *image = [UIImage imageWithData:photoData];
+    //            dispatch_async(dispatch_get_main_queue(), ^{
+    //                _userImage.image = image;
+    //            });
+    //        }
+    //    }];
+    
+    NSLog(@"y = %f", _deLabel.frame.origin.y);
+    CGSize maxSize = CGSizeMake(UI_SCREEN_W - 40, 1000);
+    CGSize contentLabelSize = [_deLabel.text boundingRectWithSize:maxSize options:NSStringDrawingTruncatesLastVisibleLine|NSStringDrawingUsesLineFragmentOrigin|NSStringDrawingUsesFontLeading attributes:@{NSFontAttributeName:_deLabel.font} context:nil].size;
+    NSLog(@"height = %f", contentLabelSize.height);
+    if (userphoto == nil) {
+        _particularsIV.image = nil;
+        CGRect rect = _header.frame;
+        rect.size.height = _deLabel.frame.origin.y + contentLabelSize.height + 20;
+        _header.frame = rect;
+        _deleteTV.tableHeaderView.frame = rect;
+        _particularsIV.hidden = YES;
+    } else {
+        CGRect rect2 = _header.frame;
+        rect2.size.height = _deLabel.frame.origin.y + contentLabelSize.height + 460;
+        _header.frame = rect2;
+        _deleteTV.tableHeaderView.frame = rect2;
+        [userphoto getDataInBackgroundWithBlock:^(NSData *photoData, NSError *error) {
+            if (!error) {
+                UIImage *image = [UIImage imageWithData:photoData];
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    _particularsIV.image = image;
+                });
+            }
+        }];
+        _particularsIV.hidden = NO;
+    }
 }
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+    
+}
+
+- (void)requestData {
+    
+    //PFQuery *query = [PFQuery queryWithClassName:@"Item"];
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"commentItem == %@",_item];
+    PFQuery *query2 = [PFQuery queryWithClassName:@"comment" predicate:predicate];
+    
+    [query2 includeKey:@"commentUser"];//关联查询
+    [query2 includeKey:@"commentItem"];//关联查询
+    [SVProgressHUD show];
+    
+    [query2 findObjectsInBackgroundWithBlock:^(NSArray *returnedObjects, NSError *error) {
+        [SVProgressHUD dismiss];
+        
+        if (!error) {
+            _objectsForShow = returnedObjects;
+            NSLog(@"_objectsForShow = %@", _objectsForShow);
+            [_deleteTV reloadData];
+        } else {
+            NSLog(@"Error: %@ %@", error, [error userInfo]);
+        }
+    }];
+}
+
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return _objectsForShow.count;;
+}
+
+// Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
+// Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    deleteTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"commentCell" forIndexPath:indexPath];
+    
+    PFObject *object = [_objectsForShow objectAtIndex:indexPath.row];
+    cell.commentUserDetail.text = [NSString stringWithFormat:@"%@", object[@"commentdetail"]];
+    
+    PFObject *activity = object[@"commentUser"];
+    
+    cell.commentName.text =[NSString stringWithFormat:@" %@   评论", activity[@"secondname"]];
+    NSLog(@"%@",activity);
+    PFFile *photo = activity[@"photo"];
+    [photo getDataInBackgroundWithBlock:^(NSData *photoData, NSError *error) {
+        if (!error) {
+            UIImage *image = [UIImage imageWithData:photoData];
+            dispatch_async(dispatch_get_main_queue(), ^{
+                cell.userImage.image = image;
+            });
+        }
+    }];
+    
+    return cell;
+    
+}
+
+
+
+
+
+
+
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
+
+{
+    
+    if (buttonIndex == 1) {
+        [self shanchu];
+        NSLog(@"YES");
+    } else {
+        NSLog(@"no");
+    }
+}
+
+- (void) shanchu {
+    
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"commentItem == %@",_item];
+    PFQuery *query2 = [PFQuery queryWithClassName:@"comment" predicate:predicate];
+    
+    [query2 findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
+        if (!error) {
+            for (PFObject *comm in objects) {
+                [comm deleteInBackground];
+            }
+        }
+    }];
+    
+    [_item deleteInBackground];
+    
+    
+    [SVProgressHUD show];
+    //判断上传成功
+    [_item saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error)/**/ {
+        [SVProgressHUD dismiss];
+        if (succeeded) {
+            [[NSNotificationCenter defaultCenter] performSelectorOnMainThread:@selector(postNotification:) withObject:[NSNotification notificationWithName:@"refreshMine" object:self] waitUntilDone:YES];//通过通知去刷新列表。自动刷新，
+            [self.navigationController popViewControllerAnimated:YES];//刷新页面
+            NSLog(@"1");
+            
+        } else {
+            [Utilities popUpAlertViewWithMsg:nil andTitle:nil];
+        }
+    }];
+    
+    
+}
+
 
 - (IBAction)deleteButtenItem:(id)sender {
     
